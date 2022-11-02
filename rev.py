4dd08e63c330e7a9312fc287e7e8464a87412d29
Hieu Tran <hieu.t.tran@sjsu.edu>
2022-11-02 15:17:55 -0700

    Updated rev.py

--- rev.py
+++ rev.py
@@ -1,9 +1,7 @@
-def reverse_list(list1):
-    if isinstance(list1, list):
-        temp = [] 
-        for e in reversed(list1):
-            temp.append(e)
-        return temp
-    else:
-        print("Invalid Input")
-        return
+def reverse_list(list):
+    temp = [] 
+    for i in range(len(list)-1,-1,-1):
+        temp.append(list[i])
+    return temp
+list = [1,4,37]
+print(reverse_list(list))
