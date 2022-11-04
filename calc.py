4ec28f672f7ac0282ff420abdb329bc6659f46e4
Hieu Tran <hieu.t.tran@sjsu.edu>
2022-11-03 22:46:41 -0700

    calc.py completed

---
+++ calc.py
@@ -0,0 +1,12 @@
+def calculator(str):
+    result = 0
+    arr = list(str)
+    stack = []
+    for i in range(len(arr)):
+        if arr[i] == "+" or "-":
+            stack.append()
+        elif arr[i] == "*" or "/" or "//":
+            stack.append()
+        else:
+            stack.append()
+    return arr
